c3a0158ea1032c849747b4a6f91f3b28220ffe2c
nsrciog <nsrciog@nsrciog.local>
2020-09-20 19:23:59 +0900

    update to show url

--- api/main.go
+++ api/main.go
@@ -6,8 +6,20 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Sample")
-    })
+    http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    method := r.Method
+    url := r.URL
+    query := r.URL.Query()
+
+    fmt.Fprintf(w, "%s %s\n", method, url)
+    if query == nil {
+        return
+    }
+    for key, val := range query {
+        fmt.Fprintf(w, "%s = %s\n", key, val[0])
+    }
 }
