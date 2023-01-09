0afc2c95002eb1ef82839133321c195e68ba18b1
enuesaa <nsrciog@gmail.com>
2023-01-09 14:51:43 +0900

    feat(api): use gin

--- api/go.mod
+++ api/go.mod
@@ -2,4 +2,7 @@ module api
 
 go 1.15
 
-require github.com/davecgh/go-spew v1.1.1
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gin-gonic/gin v1.8.2 // indirect
+)
--- api/main.go
+++ api/main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	response := createResponse(r)
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, string(response))
-}
-
-func createResponse(r *http.Request) string {
-	method := r.Method
-	url := r.URL
-	query := r.URL.Query()
-
-	return fmt.Sprintf("%s %s %s", method, url, query)
+	engine := gin.Default()
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+	engine.Run(":3000")
 }
