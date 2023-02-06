a74fd6ef7d3a71508a463199103513bd370d0b28
enuesaa <nsrciog@gmail.com>
2023-02-07 00:28:17 +0900

    feat: add sample code to invoke spotify api

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # teatime-app
+## Links
+### spotify api
+- https://zenn.dev/pp_dev/scraps/09063abc1aeaa8
--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2/clientcredentials"
+
+	"github.com/zmb3/spotify/v2"
 )
+
+var userID = ""
 
 func main() {
 	router := gin.Default()
-	router.GET("/api", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello alworld",
+	router.GET("/api", func(ctx *gin.Context) {
+		config := &clientcredentials.Config{
+			ClientID:     "",
+			ClientSecret: "",
+			TokenURL:     spotifyauth.TokenURL,
+		}
+		token, err := config.Token(context.Background())
+		if err != nil {
+			log.Fatalf("couldn't get token: %v", err)
+		}
+
+		httpClient := spotifyauth.New().Client(ctx, token)
+		client := spotify.New(httpClient)
+		user, err := client.GetUsersPublicProfile(ctx, spotify.ID(userID))
+		if err != nil {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "error",
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"message":      "hello alworld",
+			"user_id":      user.ID,
+			"display_name": user.DisplayName,
+			"spotify_uri":  string(user.URI),
+			"endpoint":     user.Endpoint,
 		})
 	})
 	router.Run(":80")
