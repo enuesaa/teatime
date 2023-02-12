e79bf62606b9a7374602039915a94813b4353408
enuesaa <nsrciog@gmail.com>
2023-02-13 02:25:21 +0900

    feat(frontend): use react-query

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,13 +2,18 @@ import type { AppProps } from 'next/app'
 import { Global, ThemeProvider } from '@emotion/react'
 import { globalStyle } from '@/styles/global'
 import { baseTheme } from '@/styles/theme'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient()
+
   return (
     <>
       <Global styles={globalStyle} />
       <ThemeProvider theme={baseTheme}>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   )
