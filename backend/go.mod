212ff7f6e44d38c6d448c077568acdfb2785d43e
enuesaa <nsrciog@gmail.com>
2023-02-12 17:00:32 +0900

    feat(gin): custom logger

--- backend/go.mod
+++ backend/go.mod
@@ -20,10 +20,10 @@ require (
 	github.com/ugorji/go/codec v1.2.8 // indirect
 	github.com/zmb3/spotify/v2 v2.3.1 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/oauth2 v0.5.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,48 +1,51 @@
 package main
 
 import (
-	"context"
-	"log"
+	"fmt"
+	"io"
 	"net/http"
-
-	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"os"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/oauth2/clientcredentials"
-
-	"github.com/zmb3/spotify/v2"
 )
 
 var userID = ""
 
 func main() {
+	// logger
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(os.Stdout, f)
+
 	router := gin.Default()
 	router.GET("/api", func(ctx *gin.Context) {
-		config := &clientcredentials.Config{
-			ClientID:     "",
-			ClientSecret: "",
-			TokenURL:     spotifyauth.TokenURL,
-		}
-		token, err := config.Token(context.Background())
-		if err != nil {
-			log.Fatalf("couldn't get token: %v", err)
-		}
+		// config := &clientcredentials.Config{
+		// 	ClientID:     "",
+		// 	ClientSecret: "",
+		// 	TokenURL:     spotifyauth.TokenURL,
+		// }
+		// fmt.Printf("aaa%v", config)
+		fmt.Fprintf(gin.DefaultWriter, "a")
+		// log.Logger.Println("aaa")
+		// token, err := config.Token(context.Background())
+		// if err != nil {
+		// 	log.Fatalf("couldn't get token: %v", err)
+		// }
 
-		httpClient := spotifyauth.New().Client(ctx, token)
-		client := spotify.New(httpClient)
-		user, err := client.GetUsersPublicProfile(ctx, spotify.ID(userID))
-		if err != nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "error",
-			})
-			return
-		}
+		// httpClient := spotifyauth.New().Client(ctx, token)
+		// client := spotify.New(httpClient)
+		// user, err := client.GetUsersPublicProfile(ctx, spotify.ID(userID))
+		// if err != nil {
+		// 	ctx.JSON(http.StatusOK, gin.H{
+		// 		"message": "error",
+		// 	})
+		// 	return
+		// }
 		ctx.JSON(http.StatusOK, gin.H{
-			"message":      "hello alworld",
-			"user_id":      user.ID,
-			"display_name": user.DisplayName,
-			"spotify_uri":  string(user.URI),
-			"endpoint":     user.Endpoint,
+			"message": "hello alworld",
+			// "user_id":      user.ID,
+			// "display_name": user.DisplayName,
+			// "spotify_uri":  string(user.URI),
+			// "endpoint":     user.Endpoint,
 		})
 	})
 	router.Run(":80")
