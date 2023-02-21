d7357ff312b9cea2cc6304bc72bfc6b69af2850f
enuesaa <nsrciog@gmail.com>
2023-02-22 02:51:42 +0900

    feat(backend): response header `Cntent-Type` should be `application/json`

--- backend/router.go
+++ backend/router.go
@@ -7,9 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// https://stackoverflow.com/questions/41109065/golang-gin-gonic-content-type-not-setting-to-application-json-with-c-json
+func JSONMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Next()
+	}
+}
+
 func CreateRouter() *gin.Engine {
 	router := gin.Default()
 
+	router.Use(JSONMiddleware())
 	base := router.Group("/api")
 	{
 		settingsRoute := base.Group("/v1.SettingsService")
