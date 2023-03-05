e29abc3572bc05d2d5237b669958c42c75f53908
enuesaa <nsrciog@gmail.com>
2023-03-05 15:41:29 +0900

    Revert "feat: api の handler を buf が generated したコードに寄せたいが go modules の知識が薄く上手くできない"
    This reverts commit 963e06854064f3a059253d14baf2dfa5e2e469e5.

--- backend/gen/v1/v1connect/settings.connect.go
+++ backend/gen/v1/v1connect/settings.connect.go
@@ -8,7 +8,7 @@ import (
 	context "context"
 	errors "errors"
 	connect_go "github.com/bufbuild/connect-go"
-	v1 "github.com/enuesaa/teatime-app/buf"
+	v1 "github.com/enuesaa/teatime-app/backend/v1"
 	http "net/http"
 	strings "strings"
 )
--- backend/main.go
+++ backend/main.go
@@ -3,29 +3,23 @@ package main
 import (
 	"io"
 	"os"
-	"net/http"
-	"golang.org/x/net/http2/h2c"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/net/http2"
 
 	"github.com/joho/godotenv"
-	"github.com/enuesaa/teatime-app/backend/gen/v1/v1connect"
 )
 
-type SettingsServer struct {
-	v1connect.UnimplementedSettingsServiceHandler
-}
-
 func main() {
-	api := http.NewServeMux()
-	api.Handle(v1connect.NewSettingsServiceHandler(&SettingsServer{}))
+	// logger
+	f, _ := os.Create("tmp/gin.log")
+	gin.DefaultWriter = io.MultiWriter(os.Stdout, f)
 
-	mux := http.NewServeMux()
-	mux.Handle("/api", http.StripPrefix("/api", api))
+	// @todo handle in dockerfile
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
 
-	_ := http.ListenAndServe(
-		"localhost:80",
-		h2c.NewHandler(mux, &http2.Server{}),
-	)
+	router := CreateRouter()
+	router.Run(":80")
 }
