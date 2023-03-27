e8cf0db88745d0dec8564be82c967308959f3df4
enuesaa <nsrciog@gmail.com>
2023-03-27 18:20:14 +0900

    chore(deps): useRouter の挙動が怪しいので nextjs を 13.1 系にダウングレードした

--- frontend/src/pages/board/items/[id].tsx
+++ frontend/src/pages/board/items/[id].tsx
@@ -3,17 +3,17 @@ import { useRouter } from 'next/router'
 
 export default function () {
   const router = useRouter()
-  // const { id } = router.query
-  // console.log(router)
-  // if (typeof id !== 'string') {
-  //   return (<></>)
-  // }
-  // console.log(id)
+  const { id } = router.query
+  console.log(router)
+  if (typeof id !== 'string') {
+    return (<></>)
+  }
+  console.log(id)
   
   return (
     <>
       <Header />
-      {/* {id} */}
+      {id}
     </>
   )
 }
