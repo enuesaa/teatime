329ea2f1ae37ff37e737041525951e213d3ff5fa
enuesaa <nsrciog@gmail.com>
2023-03-27 20:14:08 +0900

    feat(frontend): 不恰好だが api を叩くための hooks を用意した

--- frontend/src/lib/bookmark.ts
+++ frontend/src/lib/bookmark.ts
@@ -1,18 +1,7 @@
-import { createClient } from '@/lib/client'
 import { Bookmark } from '../../gen/v1/bookmark_connectweb'
-import { useState, useEffect } from 'react'
+import { queriesInit } from './use'
 
-export const useGetBookmarksQuery = () => {
-  const [data, setData] = useState<any>(null)
-
-  useEffect(() => {
-    (async () => {
-      const client = createClient(Bookmark)
-      const res = await client.getBookmark({})
-      setData(res)
-    })()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  return data
-}
+export const {
+  useQuery: useGetBookmarksQuery,
+  useLazy: useGetBookmarksLazy,
+} = queriesInit(Bookmark, async (client) => await client.getBookmark())
