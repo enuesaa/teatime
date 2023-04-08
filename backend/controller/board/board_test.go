2f2257d5224f8da386ce607fd814d62ed9eb39ee
enuesaa <nsrciog@gmail.com>
2023-04-08 17:32:54 +0900

    feat(backend): redis mock

--- backend/controller/board/board_test.go
+++ backend/controller/board/board_test.go
@@ -2,37 +2,25 @@ package board
 
 import (
 	"bytes"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"github.com/gin-gonic/gin"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/enuesaa/teatime-app/backend/repository"
 	"github.com/enuesaa/teatime-app/backend/service"
+	"github.com/stretchr/testify/assert"
 )
 
-type RedisRepositoryMock struct {}
-func (repo *RedisRepositoryMock) Keys(pattern string) []string {
-	return []string{ "board:aaa", "board:bbb" }
-}
-func (repo *RedisRepositoryMock) Delete(key string) {
-}
-func (repo *RedisRepositoryMock) JsonMget(ids []string) [][]byte {
-	list := make([][]byte, 0)
-	list = append(list, []byte(`{"id":"aaa","name":"nameaaa","url":"https://example.com/aaa"}`))
-	list = append(list, []byte(`{"id":"bbb","name":"namebbb","url":"https://example.com/bbb"}`))
-	return list
-}
-func (repo *RedisRepositoryMock) JsonGet(key string) []byte {
-	return []byte(`{"id":"aaa","name":"nameaaa","url":"https://example.com/aaa"}`)
-}
-func (repo *RedisRepositoryMock) JsonSet(key string, value interface{}) {}
-
 func boardControllerForTest() *BoardController {
-	boardSrv := service.BoardService {
-		RedisRepo: &RedisRepositoryMock {},
+	redisMock := repository.NewRedisRepositoryMock()
+	redisMock.Append("board:aaa", `{"id":"aaa","name":"nameaaa","description":""}`)
+	redisMock.Append("board:bbb", `{"id":"bbb","name":"namebbb","description":""}`)
+
+	boardSrv := service.BoardService{
+		RedisRepo: redisMock,
 	}
-	boardController := BoardController {
+	boardController := BoardController{
 		BoardSrv: &boardSrv,
 	}
 	return &boardController
@@ -42,8 +30,8 @@ func contextForTest(path string, body string) (*gin.Context, *httptest.ResponseR
 	response := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(response)
 	reqBody := bytes.NewBuffer([]byte(body))
-    req, _ := http.NewRequest("POST", path, reqBody)
-    c.Request = req
+	req, _ := http.NewRequest("POST", path, reqBody)
+	c.Request = req
 	return c, response
 }
 
@@ -51,14 +39,14 @@ func TestBoardListBoards(t *testing.T) {
 	c, response := contextForTest("/api/v1.Board/ListBoards", `{"page":1}`)
 	boardControllerForTest().List(c)
 	assert.Equal(t, 200, c.Writer.Status())
-	assert.Equal(t,`{"page":1,"items":[{"id":"aaa","name":"nameaaa","url":"https://example.com/aaa"},{"id":"bbb","name":"namebbb","url":"https://example.com/bbb"}]}`, response.Body.String())
+	assert.Equal(t, `{"page":1,"items":[{"id":"aaa","name":"nameaaa","url":"https://example.com/aaa"},{"id":"bbb","name":"namebbb","url":"https://example.com/bbb"}]}`, response.Body.String())
 }
 
 func TestBoardGetBoard(t *testing.T) {
 	c, response := contextForTest("/api/v1.Board/GetBoard", `{"id":"aaa"}`)
 	boardControllerForTest().Get(c)
 	assert.Equal(t, 200, c.Writer.Status())
-	assert.Equal(t,`{"id":"aaa","name":"nameaaa","url":"https://example.com/aaa"}`, response.Body.String())
+	assert.Equal(t, `{"id":"aaa","name":"nameaaa","url":"https://example.com/aaa"}`, response.Body.String())
 }
 
 func TestBoardAddBoard(t *testing.T) {
