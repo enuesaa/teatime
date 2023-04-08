24d0628dffdfdba824212e6122524a0753d990ad
enuesaa <nsrciog@gmail.com>
2023-04-09 00:29:53 +0900

    fix(ci): 環境によってテストの成否が変わる原因が、mapのランダム性にありそうなので、返す順番を固定する

--- backend/repository/redis_mock.go
+++ backend/repository/redis_mock.go
@@ -2,29 +2,28 @@ package repository
 
 type RedisRepositoryMock struct {
 	records map[string]string
+	keys []string
 }
 
 func NewRedisRepositoryMock() *RedisRepositoryMock {
 	return &RedisRepositoryMock{
 		records: make(map[string]string),
+		keys: make([]string, 0),
 	}
 }
 
 func (repo *RedisRepositoryMock) Append(key string, value string) {
 	repo.records[key] = value
+	repo.keys = append(repo.keys, key)
 }
 func (repo *RedisRepositoryMock) Keys(pattern string) []string {
-	keys := make([]string, 0)
-	for k := range repo.records {
-		keys = append(keys, k)
-	}
-	return keys
+	return repo.keys
 }
 func (repo *RedisRepositoryMock) Delete(key string) {
 }
 func (repo *RedisRepositoryMock) JsonMget(ids []string) [][]byte {
 	values := make([][]byte, 0)
-	for k := range repo.records {
+	for _, k := range repo.keys {
 		values = append(values, []byte(repo.records[k]))
 	}
 	return values
