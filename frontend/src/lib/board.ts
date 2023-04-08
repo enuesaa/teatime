5874daaf27fb82f39bc7332aea2287d8147ecdf4
enuesaa <nsrciog@gmail.com>
2023-04-08 14:57:33 +0900

    feat(frontend): board repository

--- frontend/src/lib/board.ts
+++ frontend/src/lib/board.ts
@@ -1,12 +1,36 @@
 import { Board } from '@/gen/v1/board_connectweb'
 import { queriesInit } from './use'
-import { AddBoardRequest } from '@/gen/v1/board_pb'
+import { AddBoardRequest, ListBoardsResponse, CheckinRequest, ArchiveBoardRequest, UnArchiveBoardRequest, ListTimelineRequest } from '@/gen/v1/board_pb'
 
 /**
  * @todo list board
  */
+export const {
+  useQuery: useListBoardsQuery,
+  useLazy: useListBoardsLazy,
+} = queriesInit<{}, ListBoardsResponse>(Board, async(client, arg) => await client.listBoards(arg))
 
 export const {
   useQuery: useGetBookmarkQuery,
   useLazy: useGetBookmarkLazy,
 } = queriesInit<AddBoardRequest, {}>(Board, async (client, arg) => await client.addBoard(arg))
+
+export const {
+  useQuery: useCheckinQuery,
+  useLazy: useCheckinLazy,
+} = queriesInit<CheckinRequest, {}>(Board, async (client, arg) => await client.checkin(arg))
+
+export const {
+  useQuery: useListTimelineQuery,
+  useLazy: useListTimelineLazy,
+} = queriesInit<ListTimelineRequest, ListBoardsResponse>(Board, async (client, arg) => await client.listTimeline(arg))
+
+export const {
+  useQuery: useArchiveBoardQuery,
+  useLazy: useArchiveBoardLazy,
+} = queriesInit<ArchiveBoardRequest, {}>(Board, async (client, arg) => await client.archive(arg))
+
+export const {
+  useQuery: useUnArchiveBoardQuery,
+  useLazy: useUnArchiveBoardLazy,
+} = queriesInit<UnArchiveBoardRequest, {}>(Board, async (client, arg) => await client.unArchive(arg))
