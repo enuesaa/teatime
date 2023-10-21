a71a59e2b0c8571dfafc2f5f9f33616c7c787e42
enuesaa <nsrciog@gmail.com>
2023-10-21 20:04:37 +0900

    feat(deps): use radixui and remove unused packages

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,20 +1,18 @@
 import type { AppProps } from 'next/app'
-import { Global, ThemeProvider } from '@emotion/react'
-import { globalStyle } from '@/styles/global'
-import { baseTheme } from '@/styles/theme'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import '@radix-ui/themes/styles.css'
+import { Theme } from '@radix-ui/themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
 
   return (
     <>
-      <Global styles={globalStyle} />
-      <ThemeProvider theme={baseTheme}>
+      <Theme>
         <QueryClientProvider client={queryClient}>
           <Component {...pageProps} />
         </QueryClientProvider>
-      </ThemeProvider>
+      </Theme>
     </>
   )
 }
