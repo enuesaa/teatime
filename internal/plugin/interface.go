9e33bd7653e3abb75b5100ebb25e07b91bc77a4f
enuesaa <nsrciog@gmail.com>
2023-10-29 03:54:02 +0900

    chore: update plugin interface

--- internal/plugin/interface.go
+++ internal/plugin/interface.go
@@ -1,26 +1,31 @@
 package plugin
 
-type FlexAtom struct {}
-type CardAtom struct {}
-
-type DashboardUi struct {
-	Name string
-	// something
-}
-type DetailUi struct {}
-
+// json schema がいいかも
+// 配列が入っていたとして ui でどう見せるかは要検討
 type Kv struct {
 	Key   string
-	Value string
+	Type string
+	StringValue string
+	IntValue int
+	BoolValue string
 }
 type Kvs []Kv
+type Info struct {
+	Name string
+	Description string
+}
+
+type Schema struct {
+	Name string
+	AllowedMethods []string // list, view, create, update, delete
+}
 
 type PluginInterface interface {
-	GetDashboardUi() (*DashboardUi, error)
-	GetDetailUi() (*DetailUi, error)
-	List() ([]*Kvs, error)
-	Detail(id string) (*Kvs, error)
-	Add(kvs Kvs) (string, error)
-	Update(id string, kvs Kvs) (string, error)
-	Delete(id string) error
+	Info() (*Info, error)
+	Schemas() ([]*Schema, error)
+	List(schema string) ([]*Kvs, error)
+	View(schema string, id string) (*Kvs, error)
+	Create(schema string, kvs Kvs) (string, error)
+	Update(schema string, id string, kvs Kvs) (string, error)
+	Delete(schema string, id string) error
 }
