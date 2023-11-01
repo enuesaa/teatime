cb7a5f7bb6b5ed146b2557b48f9002352e29d3b5
enuesaa <nsrciog@gmail.com>
2023-11-01 13:51:03 +0900

    feat(frontend): fetch cards

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { Header } from '@/components/common/Header'
 import { Container } from '@radix-ui/themes'
+import { useQuery } from 'react-query'
+
+type Cards = {
+  Items: any
+}
+
+const fetchCards = async (): Promise<Cards> => {
+  const res = await fetch('/cards')
+  const body = await res.json()
+  return body as Cards
+}
 
 export default function TopPage() {
+  const { data, error, isLoading } = useQuery('posts', fetchCards);
+  console.log(data)
+
   return (
     <>
       <Header />
