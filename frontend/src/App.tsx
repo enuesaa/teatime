099073940cf441e855f85b68ceb281e5a72feda5
enuesaa <nsrciog@gmail.com>
2023-11-01 13:14:14 +0900

    feat: use vite instead of next.js because dist may be embeded in cli binary and fewer files are best.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
-import type { AppProps } from 'next/app'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import '@radix-ui/themes/styles.css'
 import { Theme } from '@radix-ui/themes'
+import TopPage from './pages/index'
 
-export default function App({ Component, pageProps }: AppProps) {
+export const App = () => {
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
       <Theme appearance='dark'>
-        <Component {...pageProps} />
+        <BrowserRouter basename="/app">
+          <Routes>
+            <Route path='/' element={<TopPage />} />
+          </Routes>
+        </BrowserRouter>
       </Theme>
     </QueryClientProvider>
   )
