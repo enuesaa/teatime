e5ffe28820e6e64144a0c867602b17e217de4aea
enuesaa <nsrciog@gmail.com>
2023-11-03 23:36:44 +0900

    feat: update provider interface

--- pkg/plug/provider.go
+++ pkg/plug/provider.go
@@ -2,29 +2,53 @@ package plug
 
 type ProviderInterface interface {
 	Info() Info
-	ListUiCards() []UiCard
+	DescribeCard(rid string) Card
+	DescribePanel(rid string) Panel
+
+	DescribeSchema(rid string) Resource
 	List(rid string) []Resource
 	Describe(rid string) Resource
-	// Create(resource Resource) error
-	// Update(resource Resource) error
-	// Delete(resource Resource) error
-}
-
-// rid stands for resource id like arn. example: `notes:<id>` or `notes:*` or `note:main`
-type UiCard struct {
-	Layout string
-	// main layout config
-	Rid string // 
-	// CanList bool 
-	// CanCreate bool // ここが ok なら create form が表示されるみたいな
-	// CanUpdate bool
-	// CanDelete bool
+	Create(resource Resource) error
+	Update(resource Resource) error
+	Delete(resource Resource) error
 }
 
 type Info struct {
 	Name string
 	Description string
+	CardRids []string
 }
+
+type TextCardConfig struct {
+	HeadingText string
+	Center bool
+}
+
+// rid stands for resource id like arn. example: `notes:<id>` or `notes:*` or `note:main`
+// `<provider-name>:<resource-type>:<resource-name>:<id>`
+// `pinit:ui:card:main`
+type Card struct {
+	Layout string
+	TextCardConfig TextCardConfig
+	PanelRids []string
+}
+
+type TablePanelConfig struct {
+	Heading []string	
+	Values [][]TablePanelValue
+}
+type TablePanelValue struct {
+	Value Value
+	Readonly bool
+	Rid string
+}
+
+type Panel struct {
+	Layout string
+	TablePanelConfig TablePanelConfig
+	Query string // like `pinit:resource:notes:?name=`
+}
+
 
 type Resource struct {
 	Name string
