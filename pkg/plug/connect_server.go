1d572a3eea85fc5172994e2638fe90cc0a9245d3
nua <enuesaa@gmail.com>
2024-06-01 19:34:14 +0900

    refactor(plug): create NewResult function to handle errs

--- pkg/plug/connect_server.go
+++ pkg/plug/connect_server.go
@@ -6,48 +6,18 @@ type ConnectServer struct {
 
 func (s *ConnectServer) Info(arg interface{}, resp *Result[Info]) error {
 	info, err := s.Impl.Info()
-	if err != nil {
-		*resp = Result[Info]{
-			HasErr: true,
-			ErrMsg: err.Error(),
-		}
-	} else {
-		*resp = Result[Info]{
-			Data:   info,
-			HasErr: false,
-		}
-	}
+	*resp = NewResult(info, err)
 	return nil
 }
 
 func (s *ConnectServer) List(props ListProps, resp *Result[[]Tea]) error {
 	list, err := s.Impl.List(props)
-	if err != nil {
-		*resp = Result[[]Tea]{
-			HasErr: true,
-			ErrMsg: err.Error(),
-		}
-	} else {
-		*resp = Result[[]Tea]{
-			Data:   list,
-			HasErr: false,
-		}
-	}
+	*resp = NewResult(list, err)
 	return nil
 }
 
 func (s *ConnectServer) Act(props ActProps, resp *Result[string]) error {
 	message, err := s.Impl.Act(props)
-	if err != nil {
-		*resp = Result[string]{
-			HasErr: true,
-			ErrMsg: err.Error(),
-		}
-	} else {
-		*resp = Result[string]{
-			Data:   message,
-			HasErr: false,
-		}
-	}
+	*resp = NewResult(message, err)
 	return nil
 }
