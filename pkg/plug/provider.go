505e1855828b5d2fe4718255252ae55bf400d890
nua <enuesaa@gmail.com>
2024-06-01 18:20:06 +0900

    refactor(plug): change interface

--- pkg/plug/provider.go
+++ pkg/plug/provider.go
@@ -3,48 +3,55 @@ package plug
 type ProviderInterface interface {
 	Info() (Info, error)
 	List(props ListProps) ([]Tea, error)
-
-	// deprecated // ものによっては実装が難しいため
-	Get(teaid string) (Tea, error)
-
-	// Act(name string, data)
-
-	// deprecated
-	Set(tea Tea) error
-	// deprecated
-	Del(teaid string) error
-	// deprecated
-	GetCard(name string) (Card, error)
+	Act(props ActProps) error
 }
 
 type Info struct {
 	Name        string
 	Description string
 	Teaboxes    []Teabox
-	// Actions
-	// deprecated
-	Cards       []string
+	Actions     []Action
 }
-type ListProps struct {
-	TeaboxName *string
-	// lastReadTeaid // for pagination
-}
-type Tea struct {
-	Teaid  string
-	Teabox string
-	Vals   Vals
-}
-type Vals map[string]string
-type Card struct {
-	Name        string
-	Title       string
-	Description string
-	Type        string // text
-	Text        string
-}
+
 type Teabox struct {
 	Name    string
 	Comment string
-	// make slices to preserve orders
-	Vals    map[string]string // `str`, `bool`, or `int` 
+	Vals    []Val
+}
+
+type Tea struct {
+	Teaid  string
+	Teabox string
+	Vals   []Val
+}
+
+type Action struct {
+	Name    string
+	Comment string
+	Vals    []Val
+}
+
+type Val struct {
+	Name     string
+	Value    string
+	Type     ValCast // `str`, `bool`, or `int`
+	Nullable bool
+}
+
+type ValCast int
+
+const (
+	ValCastStr ValCast = iota
+	ValCastNum
+	ValCastBool
+)
+
+type ListProps struct {
+	TeaboxName *string
+	LastRead *string // for pagination
+}
+
+type ActProps struct {
+	Name string
+	Val  []Val
 }
