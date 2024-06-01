897e887e6be2e0b6fbe6fc1f2a9fc49714f3b2c4
nua <enuesaa@gmail.com>
2024-06-01 17:14:04 +0900

    feat(plug): list up refactor plan

--- pkg/plug/provider.go
+++ pkg/plug/provider.go
@@ -3,9 +3,17 @@ package plug
 type ProviderInterface interface {
 	Info() (Info, error)
 	List(props ListProps) ([]Tea, error)
+
+	// deprecated // ものによっては実装が難しいため
 	Get(teaid string) (Tea, error)
+
+	// Act(name string, data)
+
+	// deprecated
 	Set(tea Tea) error
+	// deprecated
 	Del(teaid string) error
+	// deprecated
 	GetCard(name string) (Card, error)
 }
 
@@ -13,10 +21,13 @@ type Info struct {
 	Name        string
 	Description string
 	Teaboxes    []Teabox
+	// Actions
+	// deprecated
 	Cards       []string
 }
 type ListProps struct {
 	TeaboxName *string
+	// lastReadTeaid // for pagination
 }
 type Tea struct {
 	Teaid  string
@@ -34,5 +45,6 @@ type Card struct {
 type Teabox struct {
 	Name    string
 	Comment string
-	Vals    map[string]string // `str`, `bool`, or `int`
+	// make slices to preserve orders
+	Vals    map[string]string // `str`, `bool`, or `int` 
 }
