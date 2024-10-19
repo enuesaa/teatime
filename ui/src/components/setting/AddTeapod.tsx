b34e73ae3962c0d443d0340a59e5cb92bd11f9b9
nua <enuesaa@gmail.com>
2024-10-19 18:36:06 +0900

    refactor(ui): create custom useForm to handle react-hooks-form and radix ui dialog

--- ui/src/components/setting/AddTeapod.tsx
+++ ui/src/components/setting/AddTeapod.tsx
@@ -6,23 +6,31 @@ import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { FaPlus } from 'react-icons/fa'
 
-export const AddTeapod = () => {
-  const [open, setOpen] = useState(false)
+const useAddTeapodForm = () => {
   const addTeapod = useAddTeapod()
   const form = useForm<AddReqSchema>()
 
-  const hasError = addTeapod.data?.error !== undefined
-  const submit = form.handleSubmit((data) => {
-    addTeapod.mutate(data)
-    setOpen(false)
-  })
+  const submit = form.handleSubmit((data) => addTeapod.mutate(data))
   const reset = () => {
     addTeapod.reset()
     form.reset()
   }
+  const error = addTeapod.data?.error
+  const hasError = error !== undefined
+
+  if (addTeapod.isSuccess && !hasError) {
+    reset()
+  }
+
+  return { ...form, submit, reset, error, hasError }
+}
+
+export const AddTeapod = () => {
+  const [open, setOpen] = useState(false)
+  const form = useAddTeapodForm()
 
   return (
-    <Dialog.Root open={open || hasError} onOpenChange={setOpen}>
+    <Dialog.Root open={open || form.hasError} onOpenChange={setOpen}>
       <Dialog.Trigger>
         <Button variant='outline' radius='full' mx='2' mt='1' style={{ padding: '10px' }}>
           <FaPlus />
@@ -31,25 +39,26 @@ export const AddTeapod = () => {
 
       <Dialog.Content maxWidth='450px'>
         <Dialog.Title>Add Teapod</Dialog.Title>
-        <Dialog.Description />
 
-        {hasError && 
+        {form.hasError && 
           <Callout.Root>
-            <Callout.Text>{addTeapod.data?.error}</Callout.Text>
+            <Callout.Text>{form.error}</Callout.Text>
           </Callout.Root>
         }
 
-        <form onSubmit={submit}>
+        <form onSubmit={form.submit}>
           <TextInput label='name' {...form.register('name')} />
 
           <div className={styles.actions}>
             <Dialog.Close>
-              <Button variant='soft' color='gray' onClick={reset}>
+              <Button variant='soft' color='gray' onClick={form.reset}>
                 Cancel
               </Button>
             </Dialog.Close>
 
-            <Button type='submit'>Save</Button>
+            <Dialog.Close>
+              <Button type='submit'>Save</Button>
+            </Dialog.Close>
           </div>
         </form>
       </Dialog.Content>
