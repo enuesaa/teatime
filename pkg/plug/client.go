2ec2560b9c9acf72e7a57725e922d3e2cef44b83
nua <enuesaa@gmail.com>
2025-01-16 23:57:30 +0900

    feat(plug): hclog でログを加工して転送できることを確認した

--- pkg/plug/client.go
+++ pkg/plug/client.go
@@ -1,17 +1,29 @@
 package plug
 
 import (
+	"fmt"
+	"io"
 	"os/exec"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-plugin"
 )
 
+type InternalWriter struct {
+	io.Writer
+}
+func (w *InternalWriter) Write(p []byte) (int, error) {
+	fmt.Printf("this is internal, %s", string(p))
+	return len(p), nil
+}
+
 func NewClient(command string) *plugin.Client {
 	logger := hclog.New(&hclog.LoggerOptions{
 		Name:        "teatime",
 		DisableTime: true,
 		Level:       hclog.Info,
+		Output:      &InternalWriter{},
+		JSONFormat:  true,
 	})
 	config := plugin.ClientConfig{
 		HandshakeConfig: plugin.HandshakeConfig{
